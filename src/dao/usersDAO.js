c4f824097468b4df5fb1ec043a36799c3bf2fa5b
Michał Kowalik <michal.kowalik.94+blue-michalel@gmail.com>
2019-07-27 11:09:40 +0200

    Ticket: Durable Writes

--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -61,7 +61,7 @@ export default class UsersDAO {
       // Insert a user with the "name", "email", and "password" fields.
       // TODO Ticket: Durable Writes
       // Use a more durable Write Concern for this operation.
-      await users.insertOne({ name, email, password })
+      await users.insertOne({ name, email, password }, { w: 2 })
       return { success: true }
     } catch (e) {
       if (String(e).startsWith("MongoError: E11000 duplicate key error")) {
